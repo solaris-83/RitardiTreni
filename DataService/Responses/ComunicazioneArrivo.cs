07582c832d385016e4dc72ef753f457147dcc6ec
marco <novalis83@gmail.com>
2019-12-24 18:46:57 +0100

    Replace netframework project with netstandard2.0

---
+++ DataService/Responses/ComunicazioneArrivo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataServiceLibrary.Responses
+{
+    public class ComunicazioneArrivo
+    {
+        public string comunicazione { get; set; }
+        public bool pdf { get; set; }
+    }
+}
