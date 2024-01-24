62cafdc3cb211e729a72f2e2c8ebe9c953497481
Marco Cigana <marco.cigana@capgemini.com>
2024-01-24 14:54:45 +0100

    More about python test

--- python/test.py
+++ python/test.py
@@ -1,5 +1,6 @@
 import unittest
-from Python.apitr import apitr
+from apitr import apitr
+from datetime import datetime
 
 class TestApitr(unittest.IsolatedAsyncioTestCase):
     async def test_getInfoMob(self):
@@ -13,6 +14,22 @@ class TestApitr(unittest.IsolatedAsyncioTestCase):
         a = await apitr().searchStazione('NOVARA')
         self.assertEqual(a[0]['nomeLungo'], 'NOVARA', 'Station name is different')  
         
+    async def test_getCodStazione(self):
+        a = await apitr().getCodStazione('BIELLA S.PAOLO')   
+        self.assertEqual(a, 'S00070', 'Codes do not match') 
+        a = await apitr().getCodStazione('SANTHIA`')   
+        self.assertEqual(a, 'S00240', 'Codes do not match') 
+        a = await apitr().getCodStazione('NOVARA')   
+        self.assertEqual(a, 'S00248', 'Codes do not match')  
+        
+	
+    async def test_getPartenze(self):
+        a = await apitr().getPartenze('S00070', datetime.today())
+        self.assertTrue(len(a) > 0, 'getPartenze empty list')
+        a = await apitr().getPartenze('S00240', datetime.today())
+        self.assertTrue(len(a) > 0, 'getPartenze empty list')
+        a = await apitr().getPartenze('S00248', datetime.today())
+        self.assertTrue(len(a) > 0, 'getPartenze empty list')
         
 if __name__ == "__main__":
     unittest.main()
