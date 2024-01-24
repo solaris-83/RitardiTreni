f34154bf2c7f63d2cddc37d6d78ee9fb94c759be
Marco Cigana <marco.cigana@capgemini.com>
2024-01-24 15:58:57 +0100

    unit test

--- python/test.py
+++ python/test.py
@@ -3,33 +3,42 @@ from apitr import apitr
 from datetime import datetime
 
 class TestApitr(unittest.IsolatedAsyncioTestCase):
+    
+    dict_station = {}
+    def __init__(self, methodName: str = "runTest") -> None:
+        super().__init__(methodName)
+        self.dict_station = { "BIELLA S.PAOLO": "S00070", "NOVARA": "S00248", "SANTHIA`": "S00240"}
+    
     async def test_getInfoMob(self):
-        self.assertIsNotNone(apitr(False).getInfoMob())
+        self.assertIsNotNone(await apitr(False).getInfoMob())
         
     async def test_searchStazione(self):
-        a = await apitr().searchStazione('BIELLA S.PAOLO')
-        self.assertEqual(a[0]['nomeLungo'], 'BIELLA S.PAOLO', 'Station name is different')  
-        a = await apitr().searchStazione('SANTHIA')
-        self.assertEqual(a[0]['nomeLungo'], 'SANTHIA`', 'Station name is different')
-        a = await apitr().searchStazione('NOVARA')
-        self.assertEqual(a[0]['nomeLungo'], 'NOVARA', 'Station name is different')  
+        for key in self.dict_station.keys():
+            a = await apitr().searchStazione(key)
+            self.assertEqual(a[0]['nomeLungo'], key, 'Station name is different')  
         
     async def test_getCodStazione(self):
-        a = await apitr().getCodStazione('BIELLA S.PAOLO')   
-        self.assertEqual(a, 'S00070', 'Codes do not match') 
-        a = await apitr().getCodStazione('SANTHIA`')   
-        self.assertEqual(a, 'S00240', 'Codes do not match') 
-        a = await apitr().getCodStazione('NOVARA')   
-        self.assertEqual(a, 'S00248', 'Codes do not match')  
-        
+        for key in self.dict_station.keys():
+            a = await apitr().getCodStazione(key)   
+            self.assertEqual(a, self.dict_station[key], 'Codes do not match')      
 	
     async def test_getPartenze(self):
-        a = await apitr().getPartenze('S00070', datetime.today())
-        self.assertTrue(len(a) > 0, 'getPartenze empty list')
-        a = await apitr().getPartenze('S00240', datetime.today())
-        self.assertTrue(len(a) > 0, 'getPartenze empty list')
-        a = await apitr().getPartenze('S00248', datetime.today())
-        self.assertTrue(len(a) > 0, 'getPartenze empty list')
+        for key in self.dict_station.keys():
+            a = await apitr().getPartenze(self.dict_station[key], datetime.today())
+            self.assertTrue(len(a) > 0, 'getPartenze empty list')
         
+    async def test_getArrivi(self):
+        for key in self.dict_station.keys():
+            a = await apitr().getArrivi(self.dict_station[key], datetime.today())
+            self.assertTrue(len(a) > 0, 'getArrivi empty list')
+    
+    async def test_getAndamento(self):  
+        for key in self.dict_station.keys():  
+            a = await apitr().getPartenze(self.dict_station[key], datetime.today())
+            for i in a:
+                b = await apitr().getAndamento(self.dict_station[key], str(i['numeroTreno']), apitr().timestamp2datetime(i['orarioPartenza']))
+                self.assertIsNotNone(b['numeroTreno'], 'Could not retrieve train code')
+                self.assertTrue(len(b['fermate']) > 0, 'Fermate empty list')
+            
 if __name__ == "__main__":
     unittest.main()
